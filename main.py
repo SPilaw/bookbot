46927edc9af8445ec0562db9b9d6b195e0ae7f31
SPilaw <220448852+SPilaw@users.noreply.github.com>
2025-11-10 19:11:52 +1300

    added file path argument

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from stats import count_words,count_chars,sort_charcount
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,13 +10,13 @@ def get_book_text(filepath):
     return contents
 
 def main():
-    book = "books/frankenstein.txt"
-    text = get_book_text("books/frankenstein.txt")
+    book_path = sys.argv[1]
+    text = get_book_text(book_path)
     wordcount = count_words(text)
     charcount = count_chars(text)
     sorted_list = sort_charcount(charcount)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book}...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {wordcount} total words")
     print("--------- Character Count -------")
